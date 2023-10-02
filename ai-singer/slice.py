23ad8f73cf7536a8746727610c62b4830aabc24a
ByteDance <zhangyunnian@bytedance.com>
2023-10-02 12:00:55 +0800

    fix: modify

--- ai-singer/slice.py
+++ ai-singer/slice.py
@@ -12,11 +12,11 @@ output_path = f'{os.path.dirname(__file__)}/assets/slice/'
 # 获取文件夹下所有的文件名
 file_list = list(filter(lambda item: re.match('.*\.mp3$', item), os.listdir(input_path)))
 file_list.sort()
-file_list = [file_list[0]]
+# file_list = [file_list[0]]
 
 for file_name in file_list:
     input_file = os.path.join(input_path, file_name)
     output_file = os.path.join(output_path, file_name)
 
     print(f'start handle {file_name} ...')
-    os.system(f'ffmpeg -y -i {input_file} -f segment -segment_time 15 -c copy {output_file}.%03d.mp3')
+    os.system(f'ffmpeg -y -i {input_file} -f segment -segment_time 15 -c copy {output_file}.%03d.wav')
